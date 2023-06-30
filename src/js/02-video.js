d579880ba2300221bc423ad7e9e8fa2ce8cd81bf
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-06-30 20:31:09 +0300

    finished_1)))
    123456

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+  width: 640,
+});
+
+const trotVideoCurentTime = throttle(videoCurentTime, 1000);
+player.on('timeupdate', trotVideoCurentTime);
+function videoCurentTime(event) {
+  if (event.percent < 1) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+    return;
+  }
+  localStorage.setItem('videoplayer-current-time', 0);
+}
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log('Videoplayer started from:', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+let throttle = require('lodash.throttle');
+const formRef = document.querySelector('.feedback-form');
 
+auditLocalStorage();
+
+function auditLocalStorage() {
+  const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formState) {
+    for (const key in formState) {
+      if (formState.hasOwnProperty(key)) {
+        formRef.elements[key].value = formState[key];
+      }
+    }
+  }
+}
+
+formRef.addEventListener('input', throttle(onSaveElementValue, 500));
+formRef.addEventListener('submit', onSendFeedback);
+
+const feedbackFormState = {};
+function onSaveElementValue({ target: { value, name } }) {
+  feedbackFormState[name] = value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
+function onSendFeedback(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
