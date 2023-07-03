8c7df2a59ea1cdd7025d9dc4fd0b41c66e4ad181
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-07-03 16:59:25 +0300

    done!!!)))
    1232

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+const LS_VIDEO_TIME = 'videoplayer-current-time';
 
 const player = new Player('vimeo-player', {
   id: 'vimeo-player',
@@ -11,9 +12,9 @@ const trotVideoCurentTime = throttle(videoCurentTime, 1000);
 player.on('timeupdate', trotVideoCurentTime);
 
 function videoCurentTime(event) {
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(LS_VIDEO_TIME, event.seconds);
   console.log(event.seconds);
 }
-const curentTime = localStorage.getItem('videoplayer-current-time');
+const curentTime = localStorage.getItem(LS_VIDEO_TIME);
 
 player.setCurrentTime(curentTime ? curentTime : 0);
